25536630668b07eda8f1c6a2e4687606c0b2d19b
Gabriel Cardona <gjcardonam@gmail.com>
2024-10-11 17:56:27 -0500

    Primer commit

---
+++ src/ui/Input.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Input = styled.input`
+    font-size: 20px;
+    font-weight: 400;
+    padding: 10px 20px;
+    border-radius: var(--border-radius-md);
+    border: 1px solid var(--color-grey-300);
+    background-color: var(--color-grey-0);
+    color: #000;
+`;
+
+export default Input;
